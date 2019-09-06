a01ee66e348ef749b2763919f5feeca9521e6156
liluodan <lild@wdcloud.cc>
2019-09-06 16:42:25 +0800

    feat: 构造假数据

--- README.md
+++ README.md
@@ -27,3 +27,8 @@ npm run lint
 
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
+
+### 构造假数据
+```
+vue.config.js中 添加 devServe，require('./src/data.json');其中：路径必须加上"./"，不然报错
+```
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,8 @@
+const appData = require('./src/data.json');
+const seller = appData.seller;
+const goods = appData.goods;
+const ratings = appData.ratings;
+
 module.exports = {
   css: {
     loaderOptions: {
@@ -14,5 +19,27 @@ module.exports = {
       postCompile: true,
       theme: true
     }
+  },
+  devServer: {
+    before(app) {
+      app.get('/api/seller', function (req, res) {
+        res.json({
+          errno: 0,
+          data: seller
+        })
+      });
+      app.get('/api/goods', function (req, res) {
+        res.json({
+          errno: 0,
+          data: goods
+        })
+      });
+      app.get('/api/ratings', function (req, res) {
+        res.json({
+          errno: 0,
+          data: ratings
+        })
+      })
+    }
   }
-}
+};
